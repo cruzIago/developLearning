29fdc69530216ef98d35dd31b129c4e3bc3bc22c
Iago Cruz <iagocruzgarcia@gmail.com>
2020-02-11 10:59:27 +0100

    Checker skeleton

---
+++ Assets/Scripts/blocks/Checker.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Checker : MonoBehaviour
+{
+
+    private Stack<Block> blockStack;
+    public string currentName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        blockStack = new Stack<Block>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void addBlock(Block b)
+    {
+        blockStack.Push(b);
+    }
+
+    public void removeLast()
+    {
+        blockStack.Pop();
+    }
+
+    public void removeAll()
+    {
+        while (blockStack.Count != 0)
+        {
+            removeLast();
+        }
+    }
+
+    public void checkLastBlock()
+    {
+        string blockText = blockStack.Peek().textGuide.GetComponentInChildren<Text>().text;
+        if (blockText != currentName)
+        {
+            print("EFE");
+        }
+        else
+        {
+            print("Todo ok");
+        }
+    }
+}
