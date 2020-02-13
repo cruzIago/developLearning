02f2f8834104312be9c492f217a319f357dfaaa8
Iago Cruz <iagocruzgarcia@gmail.com>
2020-02-13 12:24:22 +0100

    Well with checker
    Checker sees if the block thrown into the well is named the same as the name that they're asking for.

---
+++ Assets/Scripts/blocks/Checker.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Checker : MonoBehaviour
+{
+
+    private Stack<Block> blockStack;
+    public string currentName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        blockStack = new Stack<Block>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void addBlock(Block b)
+    {
+        blockStack.Push(b);
+    }
+
+    public void removeLast()
+    {
+        Block ejectedBlock = blockStack.Pop();
+        print(ejectedBlock.textGuide.GetComponentInChildren<Text>().text + " was poped from the stack");
+        print("Remaining blocks: " + blockStack.Count);
+    }
+
+    public void removeAll()
+    {
+        while (blockStack.Count != 0)
+        {
+            removeLast();
+        }
+    }
+
+    public bool isEmpty()
+    {
+        return blockStack.Count == 0;
+    }
+
+    public void checkLastBlock()
+    {
+        string blockText = blockStack.Peek().textGuide.GetComponentInChildren<Text>().text;
+        if (blockText != currentName)
+        {
+            print("EFE: BlockText es " + blockText + " y mi nombre es: " + currentName);
+            removeAll();
+        }
+        else
+        {
+            print("Todo ok");
+            print("Remaining blocks: " + blockStack.Count);
+        }
+    }
+}
--- Assets/Scripts/well_checker.cs
+++ Assets/Scripts/well_checker.cs
@@ -1,28 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class well_checker : MonoBehaviour
 {
-    private List<GameObject> blocks_inside;
+    private Stack<Block> blocks_inside;
+    public string askedName;
 
     private void Start()
     {
-        blocks_inside = new List<GameObject>();
+        blocks_inside = new Stack<Block>();
     }
 
-    IEnumerator check_blocks()
+    private void ejectBlocks()
     {
-        yield return new WaitForSeconds(1.0f);
-        foreach (GameObject b in blocks_inside)
+        foreach (Block b in blocks_inside)
         {
             //b.transform.Translate(new Vector3(0,2,7));
             b.GetComponent<Rigidbody>().velocity = Vector3.zero;
             b.GetComponent<Rigidbody>().AddForce(new Vector3(0f, 1f, -0.2f) * 2000f);
-            StartCoroutine(activateCollision(b));
+            StartCoroutine(activateCollision(b.gameObject));
             print("Moved");
         }
         blocks_inside.Clear();
+    }
+
+    IEnumerator check_last_block()
+    {
+        yield return new WaitForSeconds(1.0f);
+        string blockText = blocks_inside.Peek().GetComponent<Variable_block>().getVariableName();
+        if (blockText != askedName)
+        {
+            print("EFE: BlockText es " + blockText + " y mi nombre es: " + askedName);
+            ejectBlocks();
+        }
+        else
+        {
+            print("Todo ok");
+            print("Remaining blocks: " + blocks_inside.Count);
+        }
+
+        //print("Actually not doing nothing lmao");
     }
 
     IEnumerator activateCollision(GameObject other)
@@ -37,8 +56,8 @@ public class well_checker : MonoBehaviour
         if (collision.gameObject.name.Contains("block"))
         {
             Physics.IgnoreCollision(collision.gameObject.GetComponent<Collider>(), this.GetComponent<Collider>());
-            blocks_inside.Add(collision.gameObject);
-            StartCoroutine(check_blocks());
+            blocks_inside.Push(collision.gameObject.GetComponent<Block>());
+            StartCoroutine(check_last_block());
         }
     }
 }
