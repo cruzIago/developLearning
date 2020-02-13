8ce1cc28750b7c17604b7f8a06f7ee8a54446685
cruzIago <iagocruzgarcia@gmail.com>
2020-02-13 12:27:04 +0100

    UI
    Descriptions added, well has a list to check

--- Assets/Scripts/well_checker.cs
+++ Assets/Scripts/well_checker.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class well_checker : MonoBehaviour
 {
     private List<GameObject> blocks_inside;
+
+    public Text[] descriptions_to_check;
 
     private void Start()
     {
