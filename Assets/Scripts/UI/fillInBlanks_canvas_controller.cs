3a364b2e92f6bc337a0a2abca54aab04c85d5950
Iago Cruz <iagocruzgarcia@gmail.com>
2020-02-19 12:56:28 +0100

    QoL press to continue and Windows deploy

--- Assets/Scripts/UI/fillInBlanks_canvas_controller.cs
+++ Assets/Scripts/UI/fillInBlanks_canvas_controller.cs
@@ -23,7 +23,9 @@ public class fillInBlanks_canvas_controller : MonoBehaviour
 
     public InputField gui_fill_input; //For the user to make inputs to simulate code running
 
-    public Text[] result_text;
+    public Text[] result_text; //TODO
+
+    public Text pressToContinue;
 
     private void Start()
     {
@@ -102,6 +104,7 @@ public class fillInBlanks_canvas_controller : MonoBehaviour
         }
 
         yield return new WaitForSeconds(1.0f);
+        pressToContinue.gameObject.SetActive(true);
         Time.timeScale = 0;
         yield return WaitForKeyPress(KeyCode.Space);
         animator.SetBool("checking_conditions", false);
@@ -123,6 +126,7 @@ public class fillInBlanks_canvas_controller : MonoBehaviour
             {
                 isPressed = true;
                 Time.timeScale = 1;
+                pressToContinue.gameObject.SetActive(false);
             }
             yield return null;
         }
@@ -182,10 +186,12 @@ public class fillInBlanks_canvas_controller : MonoBehaviour
             last_kind_block = other.gameObject.GetComponent<Block>().kind_of_block;
             other.gameObject.GetComponent<Block>().setCollisions(true);
             other.gameObject.GetComponent<Rigidbody>().velocity = Vector3.zero;
+
             if (other.gameObject.GetComponent<Block>().player.GetComponent<player_controller>().isItemHeld)
             {
                 other.gameObject.GetComponent<Block>().pickDown();
             }
+
             other.gameObject.GetComponent<Block>().reset_position();
 
             print(user_solution.Count);
