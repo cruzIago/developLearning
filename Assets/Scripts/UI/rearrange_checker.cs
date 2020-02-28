77e9373fef54a9fd7a4ba08a4a95c86a02dae72b
cruzIago <iagocruzgarcia@gmail.com>
2020-02-28 09:27:25 +0100

    Rearrange game done
    Long commit, rearrange game practically done, victory message is missing.
    Single commit because no big changes to structure were done

---
+++ Assets/Scripts/UI/rearrange_checker.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class rearrange_checker : MonoBehaviour
+{
+    Block fixed_block;
+    public RectTransform gui_position;
+    Text ownText;
+
+    /*
+     * Sets the position of the block so it stays in place until player moves it 
+     */
+    public void fixBlock(Block block)
+    {
+        block.GetComponent<Block>().pickDown();
+        fixed_block = block;
+        fixed_block.transform.position = this.transform.position + Vector3.up;
+        fixed_block.transform.rotation = this.transform.rotation;
+        fixed_block.GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    /*
+     * For manager to check blocks and if its right 
+     */
+    public Block getBlock()
+    {
+        return fixed_block;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "item" && fixed_block == null)
+        {
+            fixBlock(other.GetComponent<Block>());
+            GetComponent<Renderer>().material.SetColor("_Color",(fixed_block.GetComponent<Control_block>().block_color)-new Color(0.5f,0.5f,0.5f,0f));
+            fixed_block.GetComponent<Control_block>().control_text.gameObject.SetActive(true);
+            fixed_block.GetComponent<Control_block>().control_text.transform.position = gui_position.transform.position;
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "item")
+        {
+            if (other.GetComponent<Block>() == fixed_block)
+            {
+                if (!fixed_block.player.GetComponent<player_controller>().isItemHeld)
+                {
+                    fixed_block.GetComponent<Rigidbody>().isKinematic = false;
+                }
+                GetComponent<Renderer>().material.SetColor("_Color", Color.grey);
+                fixed_block.GetComponent<Control_block>().control_text.gameObject.SetActive(false);
+                fixed_block = null;
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/UI/rearrange_manager.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+
+public class rearrange_manager : MonoBehaviour
+{
+    public List<Color> colors; // List of colors at manager dispossal
+    public List<rearrange_checker> platforms; //List of platforms in scene
+    public List<Block> blocks; //List of blocks in scene
+    public List<Text> texts; //List of texts on GUI to arrange
+    public List<Text> answerTexts; //List of user given answers
+    public List<RectTransform> pivots; //List of positions of texts to solve
+
+    public void Start()
+    {
+        resetControl();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            resetControl();
+        }
+        else if (Input.GetKeyDown(KeyCode.Z))
+        {
+            checkCompile();
+        }
+    }
+
+    /*
+     * It checks if code would compile or if there are errors 
+     */
+    private void checkCompile()
+    {
+        bool isAbleToCompile = true;
+        for (int i = 0; i < platforms.Count(); i++)
+        {
+            if (platforms[i].getBlock() == null
+                || !platforms[i].getBlock().Equals(blocks[i]))
+            {
+                isAbleToCompile = false;
+                break;
+            }
+        }
+
+        if (isAbleToCompile)
+        {
+            print("Correcto");
+        }
+        else
+        {
+            resetControl();
+        }
+    }
+
+    /*
+     * Randomize color list so they aren't the same color every reset
+     * Not the most efficient if done every frame, but for this time should be fine. Testing either way  
+     */
+    public void resetControl()
+    {
+        colors = colors.OrderBy(x => Random.value).ToList();
+        texts = texts.OrderBy(x => Random.value).ToList();
+        List<rearrange_checker> randomPlatforms = platforms.OrderBy(x => Random.value).ToList();
+
+        for (int i = 0; i < colors.Count(); i++)
+        {
+            texts[i].color = Color.gray;
+            texts[i].color = colors[i];
+        }
+
+        for (int i = 0; i < blocks.Count(); i++)
+        {
+            blocks[i].GetComponent<Control_block>().setColor();
+            blocks[i].GetComponent<Rigidbody>().isKinematic = false;
+            if (blocks[i].player!=null 
+                && blocks[i].player.GetComponent<player_controller>().isItemHeld)
+            {
+                blocks[i].pickDown();
+            }
+            blocks[i].transform.position = randomPlatforms[i].transform.position + (Vector3.up * 4);
+        }
+    }
+
+    /*
+     * Efficient real-time shuffle brought by Smooth.Foundations. Gonna try it if simple shuffling is too demanding on slow computers
+     * https://forum.unity.com/threads/clever-way-to-shuffle-a-list-t-in-one-line-of-c-code.241052/
+     */
+    void Shuffle<T>(IList<T> ts)
+    {
+        var count = ts.Count;
+        var last = count - 1;
+        for (var i = 0; i < last; ++i)
+        {
+            var r = UnityEngine.Random.Range(i, count);
+            var tmp = ts[i];
+            ts[i] = ts[r];
+            ts[r] = tmp;
+        }
+    }
+}
