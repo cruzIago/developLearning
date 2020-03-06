5293bb60ad26fa1d2e1c0efc46733ed436557483
Iago Cruz <iagocruzgarcia@gmail.com>
2020-03-06 12:51:25 +0100

    Main text input implementation for boss battle

--- Assets/Scripts/UI/input_textbox.cs
+++ Assets/Scripts/UI/input_textbox.cs
@@ -7,17 +7,29 @@ public class input_textbox : MonoBehaviour
 {
 
     public player_controller player;
+    public boss boss;
+    private InputField input;
+    private string currentCorrectAnswer;
 
+
+    private void Awake()
+    {
+        input = gameObject.GetComponent<InputField>();
+    }
     // Start is called before the first frame update
     void Start()
     {
+        currentCorrectAnswer = "be";//TODO
+        var submitter = new InputField.SubmitEvent();
+        submitter.AddListener(checkIfCorrect);
+        input.onEndEdit = submitter;
+
         setTextBoxActive(true);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     //Shows or hides textBox and blocks/unblocks player controller
@@ -31,6 +43,27 @@ public class input_textbox : MonoBehaviour
         } else
         {
             player.isInputBlocked = false;
+            gameObject.GetComponent<InputField>().DeactivateInputField();
         }
+    }
+
+    //Check if the answer is correct. TODO->A good checking of the input
+     void checkIfCorrect(string args0)
+    {
+        print(args0);
+        
+        gameObject.GetComponent<InputField>().DeactivateInputField();
+        if (input.text.Equals(currentCorrectAnswer))
+        {
+            boss.lifeLoss();
+        } else
+        {
+            boss.attack();
+        }
+    }
+
+    public void setCorrectAnswer(string answer)
+    {
+        currentCorrectAnswer = answer;
     }
 }
---
+++ Assets/Scripts/boss/boss.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class boss : MonoBehaviour
+{
+    private const int HITS_TO_DEFEAT = 3;
+
+    private List<string> texts;
+    public TextAsset bossPhrases;
+    public Text currentText;
+    private int indexOfCurrentText = 0;
+    private int lifeTotal = 5;
+    private int currentHits = 0;
+     
+    // Start is called before the first frame update
+    void Start()
+    {
+        texts = new List<string>();
+        appendText();//Change when needed!
+        currentText.text = texts[indexOfCurrentText];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Enemy attacks when player makes a mistake
+    public void attack() {
+        currentHits++;
+        if (currentHits == HITS_TO_DEFEAT)
+        {
+            //Game Over: Lose TODO
+            print("Fin de la partida");
+        }
+    }
+
+    //Enemy loses life when player answers correctly
+    public void lifeLoss()
+    {
+        lifeTotal--;
+        if (lifeTotal == 0)
+        {
+            //Game Over: Win TODO
+            print("Has ganado");
+        } else
+        {
+            nextText();
+        }
+    }
+
+    //Changes to next text
+    private void nextText() {
+        indexOfCurrentText++;
+        if (indexOfCurrentText < texts.Count)
+        {
+            currentText.text = texts[indexOfCurrentText];
+        }
+    }
+
+    //Loads texts from TextAsset
+    public void appendText()
+    {
+        string[] lines = bossPhrases.text.Split(';');
+        for (int i = 0; i < lines.Length; i++)
+        {
+            texts.Add(lines[i]);
+            print(texts[i]);
+        }
+    }
+}
